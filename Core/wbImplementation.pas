77efad8e4fc7a32e52ea2f68767ee2e4662221ee
robertgk2017 <robertgk2017@gmail.com>
2025-05-28 10:30:39 -0400

    Implementation \ TwbMainRecord.Init - Move AfterLoad Execution to After Required Sub-Record processing

--- Core/wbImplementation.pas
+++ Core/wbImplementation.pas
@@ -10381,8 +10381,6 @@ begin
     end;
   end;
 
-  mrDef.AfterLoad(Self);
-
   if not (mrStruct.mrsFlags.IsDeleted or GetIsPartialForm) then begin
     for i := 0 to Pred(mrDef.MemberCount) do
       if mrDef.Members[i].Required then
@@ -10402,6 +10400,8 @@ begin
       end;
     end;
   end;
+
+  mrDef.AfterLoad(Self);
 
   if wbReportMode {and mrDef.AllowUnordered} then
     if not wbSubRecordErrorsOnly or FoundError then begin
