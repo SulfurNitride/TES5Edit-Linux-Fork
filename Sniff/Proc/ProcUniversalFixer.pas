c69b6477a6096c85578dbf3ca76b0eec59b576fc
robertgk2017 <robertgk2017@gmail.com>
2025-06-13 15:28:55 -0400

    i love typos

--- Sniff/Proc/ProcUniversalFixer.pas
+++ Sniff/Proc/ProcUniversalFixer.pas
@@ -1171,7 +1171,7 @@ begin
       end else begin
         // Remove Own_Emit if not Glow Shader and Emissive Color is blank
         // Because Beth doesn't respect their own code's standards...
-        if (Shader.EditValues['Emissive Color'] = '#000000') then begin
+        if (Shader.NativeValues['Shader Flags 1\Own_Emit']) and (Shader.EditValues['Emissive Color'] = '#000000') then begin
           Shader.NativeValues['Shader Flags 1\Own_Emit'] := False;
           Log.Add(#9 + Shader.Name + ': Removed Own_Emit flag because Emissive Color is Blank');
           Result := True;
@@ -1228,7 +1228,7 @@ begin
         end;
 
       // Specular Flag
-      if (Shader.EditValues['Specular Color'] = '#000000') then begin
+      if (Shader.NativeValues['Shader Flags 1\Specular']) and (Shader.EditValues['Specular Color'] = '#000000') then begin
         Shader.NativeValues['Shader Flags 1\Specular'] := False;
         Log.Add(#9 + Shader.Name + ': Removed Specular flag because Specular Color is Blank');
         Result := True;
