297d998bedfc93e8d0cde28a7405d591e3bf97d2
robertgk2017 <robertgk2017@gmail.com>
2025-10-12 00:50:21 -0400

    Script \ Apply Custom Scripted Filter - Add missing options from the Apply Filter Form, and organization

--- Build/Edit Scripts/Apply custom scripted filter.pas
+++ Build/Edit Scripts/Apply custom scripted filter.pas
@@ -15,41 +15,87 @@ end;
 function Initialize: Integer;
 begin
   FilterConflictAll := False;
+    FilterConflictAllSet :=
+	[ // Comment out the options you Don't want
+	  caOnlyOne,         //Single Record
+	  caNoConflict,      //Multiple But No Conflict
+	  caConflictBenign,  //Benign Conflict
+	  caOverride,        //Override Without Conflict
+	  caConflict,        //Conflict
+	  caConflictCritical //Critical Conflict
+	];
+
   FilterConflictThis := False;
-  FilterByInjectStatus := False;
-  FilterInjectStatus := False;
-  FilterByNotReachableStatus := False;
-  FilterNotReachableStatus := False;
-  FilterByReferencesInjectedStatus := False;
-  FilterReferencesInjectedStatus := False;
-  FilterByEditorID := False;
-  FilterEditorID := '';
-  FilterByName := False;
-  FilterName := '';
-  FilterByBaseEditorID := False;
-  FilterBaseEditorID := '';
-  FilterByBaseName := False;
-  FilterBaseName := '';
-  FilterScaledActors := False;
-  FilterByPersistent := False;
-  FilterPersistent := False;
-  FilterUnnecessaryPersistent := False;
-  FilterMasterIsTemporary := False;
-  FilterIsMaster := False;
-  FilterPersistentPosChanged := False;
+    FilterConflictThisSet :=
+	[ // Comment out the options you Don't want
+	  ctNotDefined,                    //Not Defined
+	  ctIdenticalToMaster,             //Identical To Master
+	  ctOnlyOne,                       //Single Record
+	  ctHiddenByModGroup,              //Hidden By Mod Group
+	  ctMaster,                        //Master
+	  ctConflictBenign,                //Benign Conflict
+	  ctOverride,                      //Override Without Conflict
+	  ctIdenticalToMasterWinsConflict, //Identical To Master But Conflict Winner
+	  ctConflictWins,                  //Conflict Winner
+	  ctConflictLoses                  //Conflict Loser
+	];
+
   FilterDeleted := False;
+
+  FilterByInjectStatus := False;
+    FilterInjectStatus := False;
+
+  FilterByReferencesInjectedStatus := False;
+    FilterReferencesInjectedStatus := False;
+
+  FilterByNotReachableStatus := False;
+    FilterNotReachableStatus := False;
+
+  FilterByEditorID := False;
+    FilterEditorID := '';
+
+  FilterByName := False;
+    FilterName := '';
+
   FilterByVWD := False;
-  FilterVWD := False;
+    FilterVWD := False;
+
   FilterByHasVWDMesh := False;
-  FilterHasVWDMesh := False;
-  FilterBySignature := False;
-  FilterSignatures := '';
-  FilterByBaseSignature := False;
-  FilterBaseSignatures := '';
-  FlattenBlocks := False;
-  FlattenCellChilds := False;
+    FilterHasVWDMesh := False;
+
+  FilterByHasPrecombinedMesh := False;
+    FilterHasPrecombinedMesh := False;
+
+  FilterByPersistent := False;
+    FilterPersistent := False;
+      FilterUnnecessaryPersistent := False;
+        FilterMasterIsTemporary := False;
+        FilterIsMaster := False;
+      FilterPersistentPosChanged := False;
+
+  FlattenBlocks := True;
+  FlattenCellChilds := True;
+
   AssignPersWrldChild := False;
-  InheritConflictByParent := True; // color conflicts
+
+  InheritConflictByParent := False; // color conflicts
+
+  FilterByElementValue := False;
+    FilterElementValue := '';
+
+  FilterBySignature := False;
+    FilterSignatures := '';
+  FilterByBaseSignature := False;
+    FilterBaseSignatures := '';
+
+  FilterByBaseEditorID := False;
+    FilterBaseEditorID := '';
+
+  FilterByBaseName := False;
+    FilterBaseName := '';
+
+  FilterScaledActors := False;
+
   FilterScripted := True; // use custom Filter() function
 
   ApplyFilter;
